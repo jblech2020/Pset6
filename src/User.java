11362530829f7b6eb5696117d98231f933e0e95b
Jaedan Blechinger-Slocum <jblech2020@gmail.com>
2019-11-13 09:04:21 -0500

    Simple ATM

--- src/User.java
+++ src/User.java
@@ -1,23 +1,31 @@
 public class User {
-    
+
     private String firstName;
     private String lastName;
-    
-    ////////////////////////////////////////////////////////////////////////////
-    //                                                                        //
-    // Refer to the Simple ATM tutorial to fill in the details of this class. //
-    //                                                                        //
-    ////////////////////////////////////////////////////////////////////////////
-    
-    /**
-     * Formats the first and last name in preparation to be written to the data file.
-     * 
-     * @return a fixed-width string in line with the data file specifications.
-     */
-    
-    public String serialize() {
-        return String.format("%1$-" + ATM.FIRST_NAME_WIDTH + "s", firstName) +
-            String.format("%1$-" + ATM.LAST_NAME_WIDTH + "s", lastName);
-    }
-}
 
+    //Initializing perosanlized user variables
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //Allows access to these variables from other classes
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+
+
+    /*  Formats the first and last name in preparation to be written to the data file.
+    *   @return a fixed-width string in line with the data file specifications.
+    */
+    // public String serialize() {
+    //     return String.format("%1$-" + ATM.FIRST_NAME_WIDTH + "s", firstName) +
+    //         String.format("%1$-" + ATM.LAST_NAME_WIDTH + "s", lastName);
+    // }
+}
